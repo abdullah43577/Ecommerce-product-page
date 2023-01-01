98ecd0b8b82480a651d735b28c75b90b4d7aa96c
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-01-01 09:13:26 +0100

    optimized for mobile experience

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-"use strict"
+"use strict";
+
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".menu_items");
+const linkedLists = document.querySelectorAll(".menu_items li");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  burger.classList.toggle("active");
+
+  // Animation
+  linkedLists.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinksFade 0.5s ease forwards ${
+        index / linkedLists.length
+      }s`;
+    }
+  });
+});
